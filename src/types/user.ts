a985e80c5b17b15d75061a8706f378a57c2e50da
Taddéo Capdeboscq <tadcap@gmail.com>
2018-08-17 14:36:19 +0200

    emmario fixed the errors in src/types/user.ts by adding the missing 'this.'

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,21 +1,21 @@
 export class User {
 	public id: string
-  public first_name: string
-  public last_name: string
-  public email: string
-  public cell_phone: string
-  public country: string
-  public hospital: string
+	public first_name: string
+	public last_name: string
+	public email: string
+	public cell_phone: string
+	public country: string
+	public hospital: string
 
   constructor(public user: object)  {
   	if (user) {
-	  	this.id = user.id;
-	  	this.first_name = user.first_name;
-	  	this.last_name = user.last_name;
-	  	this.email = user.email;
-	  	this.cell_phone = user.cell_phone;
-	  	this.country = user.country;
-	  	this.hospital = user.hospital;
+	  	this.id = this.user.id;
+	  	this.first_name = this.user.first_name;
+	  	this.last_name = this.user.last_name;
+	  	this.email = this.user.email;
+	  	this.cell_phone = this.user.cell_phone;
+	  	this.country = this.user.country;
+	  	this.hospital = this.user.hospital;
   	}
   }
 }
