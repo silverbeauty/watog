c65b85d306162136a5119be6024a67d3fdf3dc1c
valeri <valeritsert@gmail.com>
2018-08-28 18:59:59 +0400

    add vote, post type

---
+++ src/types/post.ts
@@ -0,0 +1,15 @@
+import { Vote } from './vote'
+export class Post {
+	id: number
+	picture: string
+	description: string
+	category_id: number
+	user_id: number
+	vote_score: number
+	down_vote_count: number
+	up_vote_count: number
+	createdAt: Date
+	updatedAt: Date
+	downVotes: Array<Vote>
+	upVotes: Array<Vote>
+}
