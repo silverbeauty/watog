b4a3c842fc44b7302263160e92b73a377741ef3e
valeri <valeritsert@gmail.com>
2018-09-12 17:16:35 +0400

    comment for SMS/Email verification page

--- src/pages/landing/landing.ts
+++ src/pages/landing/landing.ts
@@ -37,12 +37,12 @@ export class LandingPage {
       }).then((auth: Auth) => {
         if (auth.proof_of_status) {
           if (!auth.sms_verified_date && !auth.email_verified_date) { // Email or cell_phone is not verified
-            this.navCtrl.push(RegisterThreeOfThreePage); // proof_of_status not uploaded
+            this.navCtrl.push(RegisterThreeOfThreePage); // Goto SMS/Email verify page
           } else {
             this.navCtrl.push(DashboardPage)
           }
         } else {
-          this.navCtrl.push(RegisterTwoOfThreePage)
+          this.navCtrl.push(RegisterTwoOfThreePage)  // proof_of_status not uploaded
         }
       }).catch((e: any) => {
         console.error(e)
