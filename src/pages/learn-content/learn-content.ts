b50f23a5a9caf2e7ba3f5c0f93ffcd3efa6b4d3c
luckycharm313 <webmobilepro55@aol.com>
2018-10-10 19:30:45 +0300

    chatting system
    adding left icon in learn part

--- src/pages/learn-content/learn-content.ts
+++ src/pages/learn-content/learn-content.ts
@@ -19,8 +19,55 @@ export class LearnContentPage {
   value: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.value = navParams.get('item');
-    console.log(this.value)
+    const data = navParams.get('item');
+    let _temp : any =[]
+    data.list.forEach(element => {
+      let _element: any={};
+      _element["name"] = element.name
+      _element["videos"] = element.videos
+      switch (element.name){
+        case "WATOG":
+          _element["logo"] = "../../assets/logos/WATOG.jpg"
+          break;
+        case "FIGO":
+          _element["logo"] = "../../assets/logos/figo.png"
+          break;
+        case "ACOG":
+          _element["logo"] = "../../assets/logos/ACOG.png"
+          break;
+        case "SOGC":
+          _element["logo"] = "../../assets/logos/SOGC.jpg"
+          break;
+        case "EBCOG":
+          _element["logo"] = "../../assets/logos/EBCOG.png"
+          break;
+        case "RANZCOG":
+          _element["logo"] = "../../assets/logos/RANZCOG.jpg"
+          break;
+        case "FLASOG":
+          _element["logo"] = "../../assets/logos/FLASOG.png"
+          break;
+        case "CCD":
+          _element["logo"] = "../../assets/logos/LABORATOIRE.jpg"
+          break;
+        case "BABY PROGRESS":
+          _element["logo"] = "../../assets/logos/BABY.jpg"
+          break;
+        case "Qwant Care":
+          _element["logo"] = "../../assets/logos/QWANT.png"
+          break;
+        case "FOGSI":
+          _element["logo"] = "../../assets/logos/FOGSI.png"
+          break;
+        case "COGA":
+          _element["logo"] = "../../assets/logos/COGA.jpg"
+          break;
+      }
+      _temp.push(_element);      
+    });
+    data.list = _temp;
+    this.value = data;
+    console.log("temp ", _temp);
     // const qwantCare = {
     //   name: 'Qwant Care',
     //   videos: this.value.list[0].videos
