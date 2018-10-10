db9361f7fa7b629362fcc1824e4002fa89318571
valeri <valeritsert@gmail.com>
2018-10-10 18:57:44 +0400

    fix scrollToBottom

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -204,7 +204,7 @@ export class ChatPage {
 
   scrollToBottom() {
     setTimeout(() => {
-      if (this.content.scrollToBottom) {
+      if (this.content && this.content.scrollToBottom) {
         this.content.scrollToBottom();
       }
     }, 400)
