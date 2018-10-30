d6c177f25a38a5293d755246d4dd622f9ff3a725
Taddéo Capdeboscq <tadcap@gmail.com>
2018-10-30 10:53:21 +0100

    merge fix

--- src/pages/password-reset/password-reset.module.ts
+++ src/pages/password-reset/password-reset.module.ts
@@ -3,12 +3,6 @@ import { IonicPageModule } from 'ionic-angular';
 import { PasswordResetPage } from './password-reset';
 
 @NgModule({
-<<<<<<< HEAD
-=======
-  // declarations: [
-  //   PasswordResetPage,
-  // ],
->>>>>>> e6921d80746095791f22fc52a2e77cc479a2c980
   imports: [
     IonicPageModule.forChild(PasswordResetPage),
   ],
